b235bca51709de9b3648a4db44822a4572337c31
Juan Carlos Garrido <juanc.garrido.mejia@juntadeandalucia.es>
2025-08-25 15:55:13 +0200

    Reports view

--- frontend_app/app/dashboard/histories/create/page.tsx
+++ frontend_app/app/dashboard/histories/create/page.tsx
@@ -1,5 +1,6 @@
 import Form from '@/app/ui/history/create-form';
 import Breadcrumbs from '@/app/ui/variant-analysis/breadcrumbs';
+import { InformationCircleIcon } from '@heroicons/react/24/outline';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -21,8 +22,9 @@ export default async function Page() {
         ]}
       />
 
-      <div className="p-4 pl-6 w-full h-14 bg-gray-50 rounded-xl mb-4">
-        Start a new history for a new patient.
+      <div className="p-4 pl-4 w-full h-14 bg-gray-50 rounded-xl mb-4 flex justify-between items-center">
+        <InformationCircleIcon className="h-[30] w-[30] text-gray-500" />
+        <p className="text-left w-full ml-4">Start a new history for a new patient.</p>
       </div>
 
       <Form />
--- frontend_app/app/dashboard/variant-analysis/[id]/drug-queries/[query_id]/clinical-reports/page.tsx
+++ frontend_app/app/dashboard/variant-analysis/[id]/drug-queries/[query_id]/clinical-reports/page.tsx
@@ -9,6 +9,7 @@ import { fetchFilteredReportPages } from '@/app/lib/data';
 import { Metadata } from 'next';
 import Breadcrumbs from '@/app/ui/variant-analysis/breadcrumbs';
 import Table from '@/app/ui/report/table';
+import { InformationCircleIcon } from '@heroicons/react/24/outline';
 
 export const metadata: Metadata = {
   title: 'Drug Queries',
@@ -34,25 +35,38 @@ export default async function Page(props: {
     <main>
       <Breadcrumbs
         breadcrumbs={[
-          { label: 'Variant analysis', href: '/dashboard/variant-analysis' },
+          { label: 'Patient: ENXXXXXXXXX', href: '/dashboard/' },
           {
-            label: 'Drug queries', href: `/dashboard/variant-analysis/${variantAnalysisId}/drug-queries`
+            label: 'Study: SXXXXXXXXX', href: `/dashboard2/`
           },
           {
-            label: 'Drug query', href: `/dashboard/variant-analysis/${variantAnalysisId}/drug-queries/${drugQueryId}/review`
+            label: 'Annotation: AXXXXXXXXX', href: `/dashboard3/`
           },
           {
-            label: 'Reports', href: `/dashboard/variant-analysis/${variantAnalysisId}/drug-queries/${drugQueryId}/clinical-reports`, active: true
+            label: 'Analysis: DXXXXXXXXX', href: `/dashboard4/`
+          },
+          {
+            label: 'Reports', href: `/dashboard5/`, active: true
           },
         ]}
       />
+
+      <div className="p-4 pl-4 w-full h-14 bg-gray-50 rounded-xl mb-4 flex justify-between items-center">
+        <InformationCircleIcon className="h-[30] w-[30] text-gray-500" />
+        <p className="text-left w-full ml-4">Select one report to review.</p>
+      </div>
+
+      <div className="ml-2 mr-2">
+        <Search placeholder="Search report by ID or date. Separate filters by comma." />
+      </div>
+
       <div className="w-full">
         <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
-                  <Table variant_analysis_id={variantAnalysisId} drug_query_id={drugQueryId} currentPage={currentPage} />
-                </Suspense>
-                <div className="mt-5 flex w-full justify-center">
-                  <Pagination totalPages={totalPages} />
-                </div>
+          <Table variant_analysis_id={variantAnalysisId} drug_query_id={drugQueryId} currentPage={currentPage} />
+        </Suspense>
+        <div className="mt-5 flex w-full justify-center">
+          <Pagination totalPages={totalPages} />
+        </div>
       </div>
     </main>
 
