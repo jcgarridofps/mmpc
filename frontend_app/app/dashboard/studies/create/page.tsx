97cce8720592d75c63851439be335101543ab225
Juan_Carlos_Garrido <juanc.garrido.mejia@juntadeandalucia.es>
2025-08-26 15:29:40 +0200

    Study create view

---
+++ frontend_app/app/dashboard/studies/create/page.tsx
@@ -0,0 +1,33 @@
+import Form from '@/app/ui/studies/create-form';
+import Breadcrumbs from '@/app/ui/variant-analysis/breadcrumbs';
+import { InformationCircleIcon } from '@heroicons/react/24/outline';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Create study',
+};
+
+export default async function Page() {
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Patient: ENXXXXXXXXX', href: '/dashboard/' },
+          {
+            label: 'New study',
+            href: '/dashboard2/',
+            active: true,
+          },
+        ]}
+      />
+
+      <div className="p-4 pl-4 w-full h-14 bg-gray-50 rounded-xl mb-4 flex justify-between items-center">
+        <InformationCircleIcon className="h-[30] w-[30] text-gray-500" />
+        <p className="text-left w-full ml-4">Start a new study for the patient.</p>
+      </div>
+
+      <Form />
+    </main>
+  );
+}
