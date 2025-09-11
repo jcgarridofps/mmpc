8be4d936b2950ad96e988423092e0b0940e6e049
Juan Carlos Garrido <juanc.garrido.mejia@juntadeandalucia.es>
2025-09-11 17:06:13 +0200

    Updated models

--- api/mmpc/models.py
+++ api/mmpc/models.py
@@ -1,3 +1,6 @@
+import datetime
+import random
+import string
 import uuid
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
@@ -6,6 +9,29 @@ from django.contrib.auth.models import Permission
 from django.utils import timezone
 from django.contrib.auth.models import Group
 
+#aux functions
+def generate_default_patient_id():
+    # Generate a random 12-character alphanumeric string
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=12))
+
+def generate_history_id():
+    # Generate a random 12-character alphanumeric string
+    return 'PH'.join(random.choices(string.digits, k=10))
+def generate_report_id():
+    # Generate a random 12-character alphanumeric string
+    return 'AR'.join(random.choices(string.digits, k=10))
+
+def generate_annotation_id():
+    # Generate a random 12-character alphanumeric string
+    return 'SA'.join(random.choices(string.digits, k=10))
+
+def generate_analysis_id():
+    # Generate a random 12-character alphanumeric string
+    return 'AA'.join(random.choices(string.digits, k=10))
+
+def generate_study_id():
+    # Generate a random 12-character alphanumeric string
+    return 'HS'.join(random.choices(string.digits, k=10))
 
 # Create your models here.
 
@@ -52,11 +78,98 @@ class location(models.Model):
     class Meta:
         verbose_name_plural = "location"
 
-class patient(models.Model):
 
+
+
+
+
+
+
+
+
+
+
+class computationVersion(models.Model):
+    state = models.CharField(max_length=20, unique=True)
+
+class analysisType(models.Model):
+    analysis_type = models.CharField(max_length=20, unique=True)
+
+class computationStatus(models.Model):
+    computationStatus = models.CharField(max_length=20, unique=True)
+
+class studyProdedureType(models.Model):
+    type = models.CharField(max_length=20, unique=True)
+
+class studySample(models.Model):
+    sample = models.CharField(max_length=20, unique=True)
+
+class studyPanelVersion(models.Model):
+    panelVersion = models.CharField(max_length=20, unique=True)
+
+class studyExomeCapture(models.Model):
+    exomeCapture = models.CharField(max_length=20, unique=True)
+
+
+
+class patient(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    custom_identifier = models.CharField(unique=True, max_length=60)
-    
+    appId = models.CharField(unique=True, default=generate_default_patient_id(), max_length=12, editable=False)
+    dateOfBirth = models.DateField(default=datetime.date(1900,1,1))
+
+class history(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    appId = models.CharField(unique=True, default=generate_history_id(), max_length=12, editable=False)
+    patient = models.ForeignKey(patient, on_delete=models.CASCADE)
+
+
+
+class studyProcedure(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    procedureType = models.ForeignKey(studyProdedureType, on_delete=models.CASCADE)
+    sample = models.ForeignKey(studySample, on_delete=models.CASCADE)
+    panelVersion = models.ForeignKey(studyPanelVersion, on_delete=models.CASCADE)
+    exomeCapture = models.ForeignKey(studyExomeCapture, on_delete=models.CASCADE)
+    geneList = models.JSONField()
+
+class study(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    appId = models.CharField(unique=True, default=generate_study_id(), max_length=12, editable=False)
+    description = models.CharField(max_length=200)
+    studyProcedure = models.ForeignKey(studyProcedure, on_delete=models.CASCADE)
+    date = models.DateField(auto_now=True)
+    variantsFileRoute = models.CharField(max_length=200)
+    history = models.ForeignKey(history, on_delete=models.CASCADE)
+    #sampleId = models.CharField(max_length=60)
+
+class annotation(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    appId = models.CharField(unique=True, default=generate_annotation_id(), max_length=12, editable=False)
+    study = models.ForeignKey(study, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now=True)
+    status = models.ForeignKey(computationStatus, on_delete=models.DO_NOTHING)
+    version = models.ForeignKey(computationVersion, on_delete=models.DO_NOTHING)
+    documentId = models.CharField(default='NONE', max_length=200)
+
+class analysis(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    appId = models.CharField(unique=True, default=generate_analysis_id(), max_length=12, editable=False)
+    type = models.ForeignKey(analysisType, on_delete=models.CASCADE)
+    annotation = models.ForeignKey(annotation, on_delete=models.CASCADE)
+    version = models.ForeignKey(computationVersion, on_delete=models.DO_NOTHING)
+    status = models.ForeignKey(computationStatus, on_delete=models.DO_NOTHING)
+    date = models.DateTimeField(auto_now=True)
+    cancerTypes = models.JSONField(max_length=300)
+    documentId = models.CharField(default='NONE', max_length=200)
+
+class report(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    appId = models.CharField(unique=True, default=generate_history_id(), max_length=12, editable=False)
+    analysis = models.ForeignKey(analysis, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now=True)
+    document_id = models.CharField(max_length=60, default="")
+    author = models.ForeignKey(customUser, on_delete=models.DO_NOTHING)
+
 
 class variantAnalysis(models.Model):
     STATUS_CHOICES = [
@@ -87,10 +200,4 @@ class drugQuery(models.Model):
     date = models.DateTimeField(auto_now=True)
     variant_analysis = models.ForeignKey(variantAnalysis, on_delete=models.DO_NOTHING)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='PENDING')
-    document_id = models.CharField(max_length=60, default="")
-
-class report(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    drug_query = models.ForeignKey(drugQuery, on_delete=models.DO_NOTHING)
-    date = models.DateTimeField(auto_now=True)
     document_id = models.CharField(max_length=60, default="")
