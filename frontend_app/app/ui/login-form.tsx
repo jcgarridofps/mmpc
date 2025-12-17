0f03e887371a74733aec081a41532ce118bdd137
Juan_Carlos_Garrido <juanc.garrido.mejia@juntadeandalucia.es>
2025-12-17 13:31:37 +0100

    new_login_screen

--- frontend_app/app/ui/login-form.tsx
+++ frontend_app/app/ui/login-form.tsx
@@ -20,10 +20,10 @@ export default function LoginForm() {
   );
   return (
     <form action={formAction} className="space-y-3">
-      <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
-        <h1 className={`${lusitana.className} mb-3 text-2xl`}>
+      <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-2 pt-2">
+{/*         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
           Please log in to continue.
-        </h1>
+        </h1> */}
         <div className="w-full">
           <div>
             <label
